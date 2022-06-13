d515c3be2933507041a487c6c938742a1b8fb22e
Furkan <furkan.eren@optofil.com.tr>
2022-06-13 14:30:09 +0300

    json ve pdf eklendi

--- client.py
+++ client.py
@@ -9,41 +9,124 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
+
+
+
+
+
+
+
+def getJson():
+    count = 0
+    while True:
+        s.sendto("next".encode('utf-8'), server)
+        data = s.recv(BUFFER_SIZE)
+        if (data == b'newData'):
+            recived_f = 'imgt_thread' + str(count) + '.json'
+            count += 1
+            with open(recived_f, 'wb') as f:
+                print('file opened')
+                while True:
+                    # print('receiving data...')
+                    data = s.recv(BUFFER_SIZE)
+                    # print('data=%s', (data))
+                    # print(not data)
+                    if (data == b'ok'):
+                        print('file close()')
+                        break
+                    else:
+                        f.write(data)
+                    '''
+                    if not data:
+                        f.close()
+                        print('file close()')
+                        break
+                    '''
+                    # write data to a file
+                f.close()
+        elif (data == b'finish'):
+            break
+
+def getPdf():
+    count = 0
+    while True:
+        s.sendto("next".encode('utf-8'), server)
+        data = s.recv(BUFFER_SIZE)
+        if (data == b'newData'):
+            recived_f = 'imgt_thread' + str(count) + '.pdf'
+            count += 1
+            with open(recived_f, 'wb') as f:
+                print('file opened')
+                while True:
+                    # print('receiving data...')
+                    data = s.recv(BUFFER_SIZE)
+                    # print('data=%s', (data))
+                    # print(not data)
+                    if (data == b'ok'):
+                        print('file close()')
+                        break
+                    else:
+                        f.write(data)
+                    '''
+                    if not data:
+                        f.close()
+                        print('file close()')
+                        break
+                    '''
+                    # write data to a file
+                f.close()
+        elif (data == b'finish'):
+            break
+
+
+def getImages():
+    count = 0
+    while True:
+        s.sendto("next".encode('utf-8'), server)
+        data = s.recv(BUFFER_SIZE)
+        if (data == b'newData'):
+            recived_f = 'imgt_thread' + str(count) + '.png'
+            count += 1
+            with open(recived_f, 'wb') as f:
+                print('file opened')
+                while True:
+                    # print('receiving data...')
+                    data = s.recv(BUFFER_SIZE)
+                    # print('data=%s', (data))
+                    # print(not data)
+                    if (data == b'ok'):
+                        print('file close()')
+                        break
+                    else:
+                        f.write(data)
+                    '''
+                    if not data:
+                        f.close()
+                        print('file close()')
+                        break
+                    '''
+                    # write data to a file
+                f.close()
+        elif (data == b'finish'):
+            break
+
+
+
+
 message = input("-> ")
 # inputMessage = 'getImage'
-
 s.sendto(message.encode('utf-8'), server)
-finishFlag = False
-count = 0
 time1 = time.time()
-while True:
-    s.sendto("next".encode('utf-8'), server)
-    data = s.recv(BUFFER_SIZE)
-    if(data==b'newData'):
-        recived_f = 'imgt_thread' + str(count) + '.jpeg'
-        count+=1
-        with open(recived_f, 'wb') as f:
-            print('file opened')
-            while True:
-                #print('receiving data...')
-                data = s.recv(BUFFER_SIZE)
-                #print('data=%s', (data))
-                #print(not data)
-                if(data==b'ok'):
-                    print('file close()')
-                    break
-                else:
-                    f.write(data)
-                '''
-                if not data:
-                    f.close()
-                    print('file close()')
-                    break
-                '''
-                # write data to a file
-            f.close()
-    elif(data==b'finish'):
-        break
+
+getImages()
+message = input("-> ")
+# inputMessage = 'getJson'
+s.sendto(message.encode('utf-8'), server)
+getJson()
+message = input("-> ")
+# inputMessage = 'getPdf'
+s.sendto(message.encode('utf-8'), server)
+getPdf()
 
 time2 = time.time()
 
--- server.py
+++ server.py
@@ -3,26 +3,100 @@ import time
 from threading import Thread
 import os
 
+
 TCP_IP = '192.168.1.109'
 TCP_PORT = 4000
 BUFFER_SIZE = 1024
 
 class ClientThread(Thread):
+
     def __init__(self, ip, port, sock):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.sock = sock
         print(" New thread started for "+ip+":"+str(port))
-        self.getPath()
+        self.getFiles()
 
     def getFiles(self):
         self.mainPath = "C:/Users/PC/PycharmProjects/socketP/images"
         self.files = os.listdir(self.mainPath)
 
+        self.images = []
+        self.jsonFiles = []
+        self.pdfFileList = []
+
+        for i in self.files:
+            if(i.endswith(".json")):
+                self.jsonFiles.append(i)
+            if (i.endswith(".png")):
+                self.images.append(i)
+            if (i.endswith(".jpg")):
+                self.images.append(i)
+            if (i.endswith(".jpeg")):
+                self.images.append(i)
+            if (i.endswith(".pdf")):
+                self.pdfFileList.append(i)
+
+    def sendPdfFiles(self):
+        for i in self.pdfFileList:
+            print(i)
+            while True:
+                data, addr = self.sock.recvfrom(1024)
+                data = data.decode('utf-8')
+                if (data == "next"):
+                    self.sock.send(b'newData')
+                    break
+            filename = self.mainPath + "/" + i
+            f = open(filename, 'rb')
+            self.flagDataSending = True
+            while self.flagDataSending:
+                l = f.read(BUFFER_SIZE)
+                while (l):
+                    self.sock.send(l)
+                    # print('Sent ',repr(l))
+                    l = f.read(BUFFER_SIZE)
+                if not l:
+                    print("DataSending")
+                    time.sleep(0.5)
+                    self.sock.send(b'ok')
+                    self.flagDataSending = False
+                    f.close()
+                    break
+        print('finish')
+        self.sock.send(b'finish')
+
+    def sendJsonFiles(self):
+        for i in self.jsonFiles:
+            print(i)
+            while True:
+                data, addr = self.sock.recvfrom(1024)
+                data = data.decode('utf-8')
+                if (data == "next"):
+                    self.sock.send(b'newData')
+                    break
+            filename = self.mainPath + "/" + i
+            f = open(filename, 'rb')
+            self.flagDataSending = True
+            while self.flagDataSending:
+                l = f.read(BUFFER_SIZE)
+                while (l):
+                    self.sock.send(l)
+                    # print('Sent ',repr(l))
+                    l = f.read(BUFFER_SIZE)
+                if not l:
+                    print("DataSending")
+                    time.sleep(0.5)
+                    self.sock.send(b'ok')
+                    self.flagDataSending = False
+                    f.close()
+                    break
+        print('finish')
+        self.sock.send(b'finish')
 
     def sendImage(self):
-        for i in self.files:
+        for i in self.images:
+            print(i)
             while True:
                 data, addr = self.sock.recvfrom(1024)
                 data = data.decode('utf-8')
@@ -47,7 +121,6 @@ class ClientThread(Thread):
                     break
         print('finish')
         self.sock.send(b'finish')
-
     def run(self):
         while True:
             data, addr = self.sock.recvfrom(1024)
@@ -55,6 +128,10 @@ class ClientThread(Thread):
             #print(data)
             if (data == "getImage"):
                 self.sendImage()
+            if (data == "getPdf"):
+                self.sendPdfFiles()
+            if(data == "getJson"):
+                self.sendJsonFiles()
 
 
 
